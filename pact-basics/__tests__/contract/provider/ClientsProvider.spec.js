85435906b8e1e74c276be34d664e447c51a6b18c
Alin RADU <alin.radu.cristian@gmail.com>
2025-03-10 11:59:50 +0200

    cleanup

--- pact-basics/__tests__/contract/provider/ClientsProvider.spec.js
+++ pact-basics/__tests__/contract/provider/ClientsProvider.spec.js
@@ -1,8 +1,6 @@
-const path = require('path');
 const { Verifier } = require('@pact-foundation/pact');
 const { server, importData } = require('../../../src/provider/provider');
-const { execSync } = require('child_process');
-const { getBranchName, contractTestInfo } = require('../../helpers/contractTestHelpers');
+const { contractTestInfo } = require('../../helpers/contractTestHelpers');
 
 const envFile = process.env.NODE_ENV === 'production' ? '.env.prod' : '.env.dev';
 require('dotenv').config({ path: envFile });
